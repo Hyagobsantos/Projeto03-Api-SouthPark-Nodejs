1650e01c0eb7012d176463f329cb7b13149a3b3e
Hyagobsantos <hyagobezerra64@gmail.com>
2021-09-13 12:18:57 -0300

    Cors

--- src/router/route.js
+++ src/router/route.js
@@ -1,19 +1,26 @@
 const router = require('express').Router();
 const { Router } = require('express');
 const {principal,listarTodosMoradores,listarMoradorPorId,listarTodosEpisodios,criarMorador,AtualizarMorador,deletarMotador, filtarMoradores} = require("../controller/controller");
+const cors = require("cors");
 
+//Cors
+router.use(cors());
+router.options("*", cors())
 
+//Principal
 router.get("/principal", principal);
 
+//Moradores
 router.get("/moradores", listarTodosMoradores);
 router.get("/morador/:id", listarMoradorPorId);
 router.post("/morador", criarMorador);
 router.put("/morador/:id",AtualizarMorador)
 router.delete("/morador/:id", deletarMotador)
 
+//Filtro
 router.get("/morador", filtarMoradores )
 
+//Episodeos
 router.get("/episodios", listarTodosEpisodios)
-
 
 module.exports = router;
